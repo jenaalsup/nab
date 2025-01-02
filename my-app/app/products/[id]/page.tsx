d4745711332d85dfe08117e43379098936adc73a
jenaalsup <jena.alsup@gmail.com>
2025-01-01 17:33:53 -0800

    fix broken pipeline

--- my-app/app/products/[id]/page.tsx
+++ my-app/app/products/[id]/page.tsx
@@ -43,7 +43,7 @@ export default function ProductPage() {
   return (
     <div className="max-w-4xl mx-auto p-4">
               <Navbar />
-      <Image 
+      <img 
         src={product.imageUrl}
         height={300}
         width={300}
