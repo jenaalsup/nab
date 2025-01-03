414a5d3b467ed37500b69aab30bda5c33162c2b2
Pauline Sherice Wee <paulinekowee@gmail.com>
2025-01-03 11:13:40 -0800

    login signup icons

--- my-app/app/login/page.tsx
+++ my-app/app/login/page.tsx
@@ -30,8 +30,10 @@ export default function LoginPage() {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold text-center mb-6">Log In</h1>
+      <div className="w-full max-w-md p-12 bg-white rounded border">
+      <img src="./teapot.png" alt="hero" className="w-[80px] h-auto m-auto pb-8" />
+
+        <h1 className="text-2xl font-bold text-center mb-6">log in</h1>
         <form onSubmit={handleLogin} className="space-y-4">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
--- my-app/app/signup/page.tsx
+++ my-app/app/signup/page.tsx
@@ -52,8 +52,10 @@ export default function SignUpPage() {
   
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold text-center mb-6">Sign Up</h1>
+      <div className="w-full max-w-md p-12 bg-white rounded border">
+      <img src="./bunnycat.png" alt="hero" className="w-[80px] h-auto m-auto pb-8" />
+
+        <h1 className="text-2xl font-bold text-center mb-6">sign up</h1>
         <form onSubmit={handleSignUp} className="space-y-4">
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
